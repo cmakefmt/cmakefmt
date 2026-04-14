b7f46fed0d7b07fe3afea04857c7ba0cabfb0801
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2026-04-14 18:43:12 +0100

    Replace Installation with Getting Started in header nav

--- docs/src/components/siteNav.ts
+++ docs/src/components/siteNav.ts
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 export const siteNavLinks = [
-  { label: 'Installation', href: '/installation/' },
+  { label: 'Getting Started', href: '/getting-started/' },
   { label: 'CLI', href: '/cli/' },
   { label: 'Config', href: '/config/' },
   { label: 'Performance', href: '/performance/' },
